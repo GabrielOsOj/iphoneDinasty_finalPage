e55ab13c4e0ffa0b840bdf59ad17ba26cbf5c3af
GabrielOsOj <cuentaauxiliar2@hotmail.com>
2024-11-16 15:56:10 -0300

    fixed (al fin) default selector
    ahora el selector tiene un valor por defecto
    se estan configurando las tarjetas de productos (ampliaremos :D)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,36 @@
 import { Routes } from '@angular/router';
-import { HomeComponentComponent } from './Main/homePg/home-component.component';
-import { ComparatorComponent } from './Main/comparatorPg/comparator.component';
-import { ProductComponent } from './Main/productDsPG/product.component';
+import { TestCMPComponent } from './Main/homePg/componentBk/test-cmp.component';
+import { FamilyCarrouselComponent } from './Main/homePg/productsSection/family-carrousel/family-carrousel.component';
 
 export const routes: Routes = [
-    { 
-        path: '', loadComponent:()=>import('./Main/homePg/home-component.component').then(m=>m.HomeComponentComponent)},
+    {
+        path:'test',
+        component:FamilyCarrouselComponent
+    },
+    {
+        path: '', loadComponent: () => import('./Main/homePg/home-component.component').then(m => m.HomeComponentComponent)
+    },
     {
         path: 'comparador',
         loadComponent: () => import('./Main/comparatorPg/comparator.component').then(m => m.ComparatorComponent)
     },
     {
         path: 'product/:id', loadComponent: () => import('./Main/productDsPG/product.component').then(m => m.ProductComponent)
+    },
+    {
+        path:"**",
+        redirectTo: ''
     }
+
+
 ];
+
+/*  
+ {
+        
+        path:'',
+        component:TestCMPComponent
+
+    } 
+        
+*/

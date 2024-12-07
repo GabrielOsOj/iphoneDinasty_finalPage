a3e4f887ab0aaa8784d33ce38c20b2f220d92cea
GabrielOsOj <cuentaauxiliar2@hotmail.com>
2024-12-06 21:57:48 -0300

    fixed: family carrousel
    Se agregaron los colores de los dispositivos en sus respectivas tarjetas

--- src/app/Main/shared/cards/product-card/product-card.component.ts
+++ src/app/Main/shared/cards/product-card/product-card.component.ts
@@ -3,11 +3,12 @@ import { IphoneProductColorImgs } from '../../../../Core/iphone-product';
 import { RouterLink } from '@angular/router';
 import { HammerGestureConfig, HammerModule } from '@angular/platform-browser';
 import 'hammerjs'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [RouterLink,HammerModule],
+  imports: [RouterLink,HammerModule, CommonModule],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css'
 })
@@ -15,15 +16,22 @@ export class ProductCardComponent {
 
   phoneDetails= input.required<IphoneProductColorImgs>();
   imgColorNum = input.required<number>();
+  imgSelected:string = "";
 
   constructor(private elem: ElementRef){}
 
   elementWidth = output<number>();
-  randomNum:number = 0;
   
   ngOnInit(): void {
     this.elementWidth.emit(this.elem.nativeElement.offsetWidth);
-    // this.randomNum = Math.floor(Math.random() * (this.phoneDetails().imagenes.length - 1));
+
   }
- 
+
+  colorParser():Array<string>{
+    return this.phoneDetails().imagenes.map((imgObj)=>{
+      return imgObj.imgColor
+    })
+  }
+
+
 }
